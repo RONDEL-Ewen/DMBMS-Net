114756e5d7d166781a6e1d30bc1e058e628ad2d5
Ewen Rondel <s321468@studenti.polito.it>
2025-04-04 08:46:01 -0500

    Modify __init__

---
+++ model/__init__.py
@@ -0,0 +1,8 @@
+from .components.frame_encoder          import FrameEncoder
+from .components.memory_attention       import MemoryAttention
+from .components.mask_decoder           import MaskDecoder
+from .components.hierarchical_dense_crf import HierarchicalDenseCRF
+from .components.memory_encoder         import MemoryEncoder
+from .components.memory_bank            import MemoryBank
+
+from .DMBMSNet                          import DMBMSNet
